29260f8bc2a2bbb65b0630a2f9715ca2588331a4
RomanBogdanov <lackroy5111@gmail.com>
2023-08-12 20:45:26 +0700

    Added negative subscription test

--- users/tests.py
+++ users/tests.py
@@ -65,3 +65,51 @@ class SubscriptionPositiveTestCase(APITestCase):
         self.assertFalse(
             Subscription.objects.all().exists(),
         )
+
+
+class SubscriptionNegativeTestCase(APITestCase):
+    
+    def setUp(self) -> None:
+        
+        self.user = User.objects.create(
+            email='test@test.com',
+        )
+        self.user.set_password('test')
+        self.user.save()
+        
+        self.course = Course.objects.create(
+            name='test course',
+            description='test course description',
+        )
+
+        self.lesson = Lesson.objects.create(
+            name='test',
+            description='test description',
+            video_url='https://www.youtube.com',
+            owner=self.user,
+            course=self.course,
+        )
+    
+    def test_subscription_create(self):
+    
+        self.client.force_authenticate(user=self.user)
+        
+        data = {
+            'course': self.course.pk,
+            'user': self.user.pk,
+        }
+        
+        response = self.client.post(
+            path='/user/sub/create/', data=data,
+        )
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        response = self.client.post(
+            path='/user/sub/create/', data=data,
+        )
+        
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        
+        
